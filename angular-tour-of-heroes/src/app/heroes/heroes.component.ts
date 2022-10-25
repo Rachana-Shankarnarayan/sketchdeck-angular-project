2ff21ba775a490804fb0ff8032ce84024a3b904b
Rachana-Shankarnarayan <103790742+Rachana-Shankarnarayan@users.noreply.github.com>
2022-10-25 00:09:39 -0700

    Finished step11 of test project (#1)

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  
+  heroes: Hero[] = [];
+  
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    const heroIds = [12, 13, 14, 15, 16, 17, 18, 19, 20];
+    for (var id of heroIds) {
+      this.heroService.getHero(id).subscribe(resp => this.heroes.push(resp))
+    }
+
+    // this.heroes = this.heroService.getHeros();
+  }
+
+}
